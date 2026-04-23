80c274f2625afefd9e2812ee39da6e8d0cfe30c1
Pedro Dantas <pedro2008hds@gmail.com>
2026-04-22 21:31:52 -0300

    pilulas aula de programação
    Este repositório contém materiais introdutórios para o aprendizado da linguagem Python, abordando desde conceitos básicos até exemplos práticos para fixação.

---
+++ pilula1.py
@@ -0,0 +1,33 @@
+def ranking_prioridade(pacientes):
+    ranking = []
+    for paciente in pacientes:
+        pontos = 0
+        if paciente ['gravidade'] >= 4 :
+            pontos +=3
+        elif paciente ['gravidade'] >=2:
+            pontos +=2
+        if paciente ['idade'] >=60:
+            pontos +=2
+        ranking.append({'nome':paciente['nome'],'pontos':pontos})
+    #bubble sort
+    for i in range(len(ranking)):
+        for j in range(i + 1,len(ranking)):
+            if ranking[i]['pontos'] < ranking[j]['pontos']:
+                ranking[i], ranking[j] = ranking[j], ranking[i]
+
+    for i in range(len(ranking)):
+        print(f'{i+1} - {ranking[i]['nome']},{ranking[i]['pontos']}')
+    
+def main():
+    pacientes = [
+        {'nome':'Ana','idade':70, 'gravidade': 3},
+        {'nome':'Carlos','idade':40, 'gravidade': 5},
+        {'nome':'Beatriz','idade':65, 'gravidade': 2},
+        {'nome':'João','idade': 30,'gravidade': 1}
+    ]
+
+    print(pacientes[0]['nome'])
+    ranking_prioridade(pacientes)
+    
+
+main()
---
+++ pilula5.py
@@ -0,0 +1,34 @@
+def processar_consultas(registros):
+    tempos = {}
+    cont = {}
+    status = {}
+
+    for reg in registros:
+        p = reg['paciente']
+        if p not in tempos:
+            tempos[p] = 0
+            cont [p] = 0
+        tempos[p] += reg['tempo']
+        cont[p] += 1
+
+    for paciente in tempos:
+        t = tempos[paciente]
+        if t < 2:
+            status[paciente] = 'leve'
+        elif t < 5:
+            status[paciente] = 'moderado'
+        else:
+            status[paciente] = 'crítico'
+
+    for paciente in tempos:
+        print(f'Paciente: {paciente}, Tempo total {tempos[paciente]}, Qtd de consultas {cont[paciente]}, Status: {status[paciente]}')
+
+def main():
+    registros = [
+        {'paciente':'Ana','tempo': 1},
+        {'paciente':'Ana','tempo': 2},
+        {'paciente':'Carlos','tempo': 4}
+    ]
+    processar_consultas(registros)
+
+main()
